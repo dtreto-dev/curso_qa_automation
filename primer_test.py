a63d17ef3dc29ea457e9cdd331fed8d7ebf07518
David Treto <d.treto.ur@gmail.com>
2026-06-08 23:43:15 -0300

    Se instala pytest y se agrega primer caso de prueba automatizado

--- primer_test.py
+++ primer_test.py
@@ -1,9 +1,20 @@
-def check_status_code(status):
-    if status == 200:
-        return "¡Prueba Exitosa! La API respondió OK."
+# Esta es la función del sistema que vamos a testear
+def validar_usuario(usuario):
+    if usuario == "david_qa":
+        return "Acceso Concedido"
     else:
-        return f"¡Error en la prueba! Código detectado: {status}"
+        return "Usuario Incorrecto"
 
-# Simulamos que probamos una API y nos devuelve un 200
-resultado_de_la_prueba = check_status_code(200)
-print(resultado_de_la_prueba)
+
+# --- AQUÍ EMPIEZAN LOS TESTS DE QA ---
+# Recordá que en Pytest las funciones siempre empiezan con "test_"
+
+def test_login_exitoso():
+    resultado = validar_usuario("david_qa")
+    # Aseguramos que con el usuario correcto de acceso
+    assert resultado == "Acceso Concedido"
+
+def test_login_fallido():
+    resultado = validar_usuario("intruso123")
+    # Aseguramos que con un impostor rebote
+    assert resultado == "Usuario Incorrecto"
