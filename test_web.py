71231cd368ee9f05f7891b4a70a42d475149fb8e
David Treto <d.treto.ur@gmail.com>
2026-06-11 18:09:19 -0300

    Se automatiza flujo de remover producto y validacion de elemento oculto en modo slow-mo

--- test_web.py
+++ test_web.py
@@ -1,24 +1,31 @@
 import pytest
-from playwright.sync_api import Page
+from playwright.sync_api import sync_playwright, expect
 
-def test_agregar_producto_al_carrito(page: Page):
-    # 1. Login clásico (lo que ya dominás)
-    page.goto("https://www.saucedemo.com")
-    page.locator("#user-name").fill("standard_user")
-    page.locator("#password").fill("secret_sauce")
-    page.locator("#login-button").click()
-    
-    # 2. ACCIÓN NUEVA: Buscamos el botón del primer producto y hacemos CLIC
-    # Usamos el ID del botón específico de la mochila "Sauce Labs Backpack"
-    page.locator("#add-to-cart-sauce-labs-backpack").click()
-    
-    # 3. Freno de mano de 2 segundos para ver el cambio con tus propios ojos
-    page.wait_for_timeout(2000)
-    
-    # 4. VALIDACIÓN DE QA: Buscamos el texto que está adentro del círculo del carrito
-    # La clase web del contador se llama ".shopping_cart_badge"
-    texto_carrito = page.locator(".shopping_cart_badge").text_content()
-    
-    # Aseguramos que el robot lea un "1" adentro del carrito
-    assert texto_carrito == "1"
-    print(f"\n¡Test Exitoso! El carrito tiene {texto_carrito} producto adentro.")
+def test_remover_producto_del_carrito():
+    # Iniciamos Playwright de forma manual para controlar la velocidad
+    with sync_playwright() as p:
+        # Lanzamos el navegador en modo visible (--headed) y con cámara lenta de 1.5 segundos
+        browser = p.chromium.launch(headless=False, slow_mo=1500)
+        page = browser.new_page()
+        
+        # 1. Login
+        page.goto("https://www.saucedemo.com")
+        page.locator("#user-name").fill("standard_user")
+        page.locator("#password").fill("secret_sauce")
+        page.locator("#login-button").click()
+        
+        # 2. Agregamos el producto al carrito
+        page.locator("#add-to-cart-sauce-labs-backpack").click()
+        
+        # 3. Hacemos clic en el botón de Remover
+        page.locator("#remove-sauce-labs-backpack").click()
+        
+        # 4. VALIDACIÓN DE QA AVANZADA: 
+        # Aseguramos que el contador NO esté visible.
+        contador_carrito = page.locator(".shopping_cart_badge")
+        expect(contador_carrito).to_be_hidden()
+        
+        print("\n¡Test Exitoso! El producto se eliminó en cámara lenta.")
+        
+        # Cerramos el navegador ordenadamente
+        browser.close()
